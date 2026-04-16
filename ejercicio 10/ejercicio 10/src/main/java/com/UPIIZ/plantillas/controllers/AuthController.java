4b49bc1091507e2c971a3d4280cd330edb191c66
marvyn12 <tuemail@ejemplo.com>
2026-04-15 21:21:36 -0600

    Subiendo archivos reales del ejercicio 10

---
+++ ejercicio 10/ejercicio 10/src/main/java/com/UPIIZ/plantillas/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package com.UPIIZ.plantillas.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("auth")
+public class AuthController {
+    //http://localhost:8080/auth/login
+    //http://localhost:8080/auth/registrer
+
+    //de las plantilas encontrar
+    // la carpeta css
+    // la carpeta js
+    //la carpeta plugins
+    @GetMapping("/login")
+    public String login()
+    {
+        return "login";
+    }
+    @GetMapping("/register")
+    public String register()
+    {
+        return "register";
+    }
+    @GetMapping("/forgot-password")
+    public String forgot() {return "forgot-password";}
+}
