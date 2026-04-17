968dfaec49a6f8955f8a192e78e66290d0f526e7
marvyn12 <tuemail@ejemplo.com>
2026-04-16 23:18:16 -0600

    Agregando ejercicio 10 con Dockerfile y vistas de AdminLTE

---
+++ ejercicio10/src/main/java/com/upiiz/ejercicio10/controllers/AuthController.java
@@ -0,0 +1,25 @@
+package com.upiiz.ejercicio10.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/forgot-password")
+    public String forgot() {
+        return "forgot-password";
+    }
+}
