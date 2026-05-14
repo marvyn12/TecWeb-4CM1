f9434eb81919146cdd15efad7cbdce633337ede8
Marvyn Briseño <159955768+marvyn12@users.noreply.github.com>
2026-05-14 14:50:45 -0600

    Add files via upload

---
+++ calculadoraBasica/src/test/java/com/upiiz/calculadoraBasica/CalculadoraBasicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upiiz.calculadoraBasica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraBasicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
