fe2cc2c6a6d79a3c1ae60937eb7d3b886a7b5bc2
Marvyn Briseño <159955768+marvyn12@users.noreply.github.com>
2026-05-14 11:54:36 -0600

    Add files via upload

---
+++ cientifica/cientifica/src/test/java/com/upiiz/cientifica/CientificaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upiiz.cientifica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CientificaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
