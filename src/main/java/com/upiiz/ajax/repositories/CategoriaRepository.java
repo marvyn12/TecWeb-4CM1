8d092dc08f76521e8f663ac5437715b22488b6c4
Marvyn Briseño <159955768+marvyn12@users.noreply.github.com>
2026-06-07 17:40:16 -0600

    Delete src directory

--- src/main/java/com/upiiz/ajax/repositories/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.upiiz.ajax.repositories;
-
-import com.upiiz.ajax.entities.CategoriaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<CategoriaEntity, Long> {
-}
--- src/main/java/com/upiiz/ajax/repositories/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.upiiz.ajax.repositories;
-
-import com.upiiz.ajax.entities.ProductoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<ProductoEntity, Long> {
-}
